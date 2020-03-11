728bc7f3bd86d84978e5689936f3a972f2ba1d7f
NicoRmrz <nicoramirez72@yahoo.com>
2020-03-11 16:46:24 -0700

    Add files via upload

--- scriptParser.py
+++ scriptParser.py
@@ -1,6 +1,8 @@
 import os
 from PyQt5.QtGui import QImage
 from PyQt5.QtCore import Qt, QThread, pyqtSignal, pyqtSlot, QObject, QFile
+import json
+
 
   # Class: scriptParser
 #       Functions to parse input script
@@ -14,7 +16,15 @@ class scriptParser(QObject):
 	def parseScriptFile(self, pathToFile):
 		success = False
 		Script = QFile(pathToFile)
-		print(pathToFile)
+
+		with open(pathToFile, 'r') as f:
+			datasheet_dict = json.load(f)
+
+		for distro in datasheet_dict:
+			print(distro['Name'])
+
+		with open('test/outdata.txt', 'w') as outfile:
+			json.dump(datasheet_dict, outfile)
 
 		success = True
 		return success
